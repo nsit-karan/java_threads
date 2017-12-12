ad4f0d842f5a8fcbb62b90e61302eef70c944925
nsit_karan <nsit_karan@yahoo.com>
2017-12-12 10:49:48 -0800

    partial thread code (in-progress)

---
+++ src/com/threads/java/App.java
@@ -0,0 +1,22 @@
+package com.threads.java;
+
+public class App {
+
+	public class Runner extends Thread {
+
+		@Override
+		public void run() {
+			System.out.println("Inside thread 1");
+		}
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
