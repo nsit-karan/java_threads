e2c8276af7a910e73772225fa08e6a86b4e2af61
nsit_karan <nsit_karan@yahoo.com>
2017-12-24 08:25:28 -0800

    basic stub code for producer consumer using basic java methods

---
+++ src/com/threads/producersConsumers/nativeConstructs/Processor.java
@@ -0,0 +1,17 @@
+package com.threads.producersConsumers.nativeConstructs;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Processor {
+	
+	private List<Integer> list = new LinkedList<Integer>();
+
+	public void produce() throws InterruptedException {
+		
+	}
+	
+	public void consume() throws InterruptedException {
+		
+	}
+}
